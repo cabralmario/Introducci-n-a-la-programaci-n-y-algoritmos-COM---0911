bb96dda0e679c836c4fb8f8b86ed703b228dd5fc
cabralmario <cabralmario59@gmail.com>
2026-09-01 08:25:58 -0300

    se modificó ejercicio 5

--- Actividad String_clase_3/grupo1_ejercicio04.py
+++ Actividad String_clase_3/grupo1_ejercicio04.py
@@ -11,19 +11,21 @@
 
 usuario_dni = input("Ingrese su DNI, máximo 8 dígitos: ")
 
-if  len(usuario_dni) > 8:
-    print("Error, solo 8 dígitos")
-else:
-    print(f"Correcto. Tu DNI es: {usuario_dni}")
-
-
 primer_digito = usuario_dni[0]
 ultimo_digito = usuario_dni[-1]
 
 
 print(f"Tu primer dígito del DNI es: {primer_digito}")
 print(f"Tu último dígito del DNI es: {ultimo_digito}")
-print(f"Tus tres dígitos  del DNI es: {usuario_dni[2]} {usuario_dni[3]} {usuario_dni[4]}")
+print(f"Tus tres dígitos  del DNI es: {usuario_dni[2]}, {usuario_dni[3]}, {usuario_dni[4]}")
+
+
+# if  len(usuario_dni) > 8:
+#     print("Error, solo 8 dígitos")
+# else:
+#     print(f"Correcto. Tu DNI es: {usuario_dni}")
+
+
 
 
 
