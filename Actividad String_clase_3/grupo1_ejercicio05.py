d0fefc0305a726d40cb98ec3d381a348600f0f30
cabralmario <cabralmario59@gmail.com>
2026-09-01 21:43:41 -0300

    Realice el ejercicio 5 y 6

--- Actividad String_clase_3/grupo1_ejercicio05.py
+++ Actividad String_clase_3/grupo1_ejercicio05.py
@@ -8,3 +8,14 @@
 # a) Solicitar la fecha en formato compacto.
 # b) Separar el año, el mes y el día usando cortes.
 # c) Mostrar la fecha en el formato DD/MM/AAAA.
+
+fecha_compacta = input("Ingrese la fecha sin espacio ni separadores (AAAAMMDD): ")
+
+año= fecha_compacta[0:4]
+mes= fecha_compacta[4:6]
+día= fecha_compacta[6:8]
+
+print(f"La fecha en formato DD/MM/AAAA es: {día}/{mes}/{año}")
+
+
+
--- Actividad String_clase_3/grupo1_ejercicio06.py
+++ Actividad String_clase_3/grupo1_ejercicio06.py
@@ -9,3 +9,10 @@
 # b) Construir la dirección de correo respetando la regla.
 # c) Mostrar la dirección obtenida.
 
+nombre = input("Ingresa tu nombre: ")
+apellido = input("Ingresa tu apellido: ")
+
+nombre_apellido = nombre+ "." +apellido 
+nombre_apellido_minus = nombre_apellido.lower().strip()
+print(f"Tu correo electronico es: {nombre_apellido_minus}@unpilar.edu.ar")
+
