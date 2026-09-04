50282e284bcc5b943742c3e6363b17fbb3ed28fe
cabralmario <cabralmario59@gmail.com>
2026-09-04 00:02:26 -0300

    Agrego ejercicio 7

--- Actividad String_clase_3/grupo1_ejercicio07.py
+++ Actividad String_clase_3/grupo1_ejercicio07.py
@@ -8,3 +8,11 @@
 # a) Solicitar un teléfono tal como figura en la base.
 # b) Eliminar los espacios, los guiones y los paréntesis.
 # c) Mostrar el teléfono depurado y la cantidad de dígitos que quedaron
+
+numero_telefono = input("Ingrese su telefono eje: (011) 1265-1235:  ")
+
+
+numero_telefono_depurado = numero_telefono.replace("(", "").replace(")", "").replace("-", "").replace(" ", "")
+cantidad_digitos_telefono_limpio = numero_telefono_depurado.__len__()
+
+print(f"Su número de telefono es: {numero_telefono_depurado} // Cantidad de dígitos: {cantidad_digitos_telefono_limpio}" )
